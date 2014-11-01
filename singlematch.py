d0bb8e1deb0e02fe24db6eb63c0e4f657faeb070
liuyi103 <liuyicheng1991@hotmail.com>
2014-11-01 21:13:18 +0800

    right code

--- singlematch.py
+++ singlematch.py
@@ -2,10 +2,10 @@ import networkx as nx
 import random
 import matplotlib.pyplot as plt
 import numpy as np
-g=nx.random_graphs.binomial_graph(1000, 0.1)
+g=nx.random_graphs.binomial_graph(1000, 0.001)
 p1=0.2#bad match
 p2=0.5#back to market
-paces=[2,5,10,20,50,100,200,500,1000]
+paces=range(10,1010,10)
 ans={i:[]for i in paces}
 for k in range(10):
     print k
@@ -13,7 +13,7 @@ for k in range(10):
         pos=pace
         cur=[i for i in g.nodes() if i<pos]
         bad=0
-        while pos<1001:
+        while True:
             match=nx.maximal_matching(g.subgraph(cur))
             for i in match:
                 for j in i:
@@ -24,8 +24,12 @@ for k in range(10):
                         bad+=1
                         cur.remove(j)
             cur+=range(pos,pos+pace)
-            pos+=pace
-        bad+=len(cur)-pace
+            if pos>=1000:
+                break
+            pos=pos+pace
+        bad+=len([i for i in cur if i<1000])
         ans[pace]+=[bad]
 plt.plot(paces,[np.mean(ans[i]) for i in paces])
+plt.xlabel('pace')
+plt.ylabel('bad guys')
 plt.show()
